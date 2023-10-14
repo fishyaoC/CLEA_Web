eccbd6de46275ca8c8fa1fd174de75bc194cf1f2
p3344278 <105224687+p3344278@users.noreply.github.com>
2023-10-15 03:04:51 +0800

    update

---
+++ Clea_Web/Models/ViewBookEvaluationP.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Clea_Web.Models;
+
+public partial class ViewBookEvaluationP
+{
+    public Guid MId { get; set; }
+
+    public int? MIndex { get; set; }
+
+    public string? MName { get; set; }
+
+    public string? MPublish { get; set; }
+
+    public string? MNumber { get; set; }
+
+    public DateTime? MVersion { get; set; }
+
+    public Guid? FMatchKey { get; set; }
+}
